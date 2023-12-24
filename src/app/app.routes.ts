41fcf67b2f696b92b10db2537239ed6ed4efbd4a
Storisi <stiivenmendez16@gmail.com>
2023-12-24 13:09:12 -0500

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
+];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
+  imports: [FormsModule],
+  standalone: true,
 })
 export class LoginComponent {
+  error: string | null = null;
 
+  constructor(private router: Router) {}
+
+  onSubmit(form: NgForm): void {
+    if (form.valid) {
+      const data: any = form.value;
+      if (data.email === 'test@example.com' && data.password === 'password') {
+        this.router.navigate(['/dashboard']);
+      } else {
+        this.error = 'Invalid credentials';
+      }
+    }
+  }
 }
